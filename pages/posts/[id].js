a1df575ed6281fdb39e9a6de26fcfcf2ca5a2742
Myad Tahajody <myad.tahajody@tretton37.com>
2020-06-26 14:51:23 +0200

    Add testing post (#5)
    * Add testing post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,10 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import ReadTime from '../../components/readtime'
-import utilStyles from '../../styles/utils.module.css'
+import Head from "next/head";
+
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import ReadTime from "../../components/readtime";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
@@ -21,22 +22,22 @@ export default function Post({ postData }) {
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
