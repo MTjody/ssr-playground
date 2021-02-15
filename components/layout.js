b3839bbed69e35446aa8e83f8e357b38ca0cf866
Myad Tahajody <10709345+MTjody@users.noreply.github.com>
2021-02-16 00:31:58 +0100

    Update favicon

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <meta
           name="description"
           content="Yeah I'll probably update this one soon™"
